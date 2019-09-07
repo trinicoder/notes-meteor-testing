f126b7ac7257db20c60b942d8c5a1d3da6b68a02
Varoon Silochan <varoonx@gmail.com>
2019-09-07 18:13:49 -0400

    NoteList Component setup

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import PrivateHeader from './PrivateHeader';
 
+import NoteList from './NoteList';
 export default () => {
     return (
       <div>
         <PrivateHeader title={"Dashboard"}/>
         <div className="page-content">            
-          Dashboard page content.
+
+          <NoteList/>
           </div>        
       </div>
     );
